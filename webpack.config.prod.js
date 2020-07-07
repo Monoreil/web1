f9eb0f3cc988a91aa994947b81526dadead38c14
hyeongmin <dkzmskslxm@naver.con>
2020-07-07 17:49:21 +0900

    M

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 module.exports = {
     mode: "production",
-    entry: "./src/index.js",
+    entry: {
+        index: "./src/index.js",
+        second: "./src/second.js",
+        third:"./src/third.js"
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "index_bundle.js"
+        filename: "[name]_bundle.js"
     },
     module: {
         rules: [
