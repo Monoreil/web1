8706c2e1e230afec0c23953931e9a3235d5683da
hyeongmin <dkzmskslxm@naver.con>
2020-07-08 09:05:49 +0900

    M

--- src/third.js
+++
@@ -1,9 +0,0 @@
-import RowOption from "./RowOption.js"
-import ColOption from "./ColOption.js"
-import SplitWin from "./SplitWin.js";
-
-var rowOption = Object.assign({},RowOption,{wid1:"#first",wid2:"#second"});
-var colOption = Object.assign({},ColOption,{wid1:"#third",wid2:"#forth"});
-
-new SplitWin(rowOption).makeGrid();
-new SplitWin(colOption).makeGrid();
